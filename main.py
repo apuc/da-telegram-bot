baea29345e62e41ebc612251b28418e167baf7eb
Kavalar <apuc06@mail.ru>
2024-04-19 14:45:28 +0300

    main fix

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ class DaBotDaemon(Daemon):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # with DaBotDaemon('/tmp/daemon-dabot.pid', error_log_file='errlog.txt') as daemon:
-    #     daemon.process_command()
-    da = DaBot()
-    da.run()
+    with DaBotDaemon('/tmp/daemon-dabot.pid', error_log_file='errlog.txt') as daemon:
+        daemon.process_command()
+        # da = DaBot()
+        # da.run()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
